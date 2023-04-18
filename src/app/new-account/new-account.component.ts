9351df1415262e67a0303d4bb720f51dcb17c7bd
Lord, Acacia [USA] <Lord_Acacia@bah.com>
2023-04-18 12:51:50 -0400

    Injecting the logging service into components

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,19 +1,26 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-// import { LoggingService } from '../logging.service';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+
+  constructor(private loggingService: LoggingService) {
+
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
+
+    this.loggingService.logStatusChange(accountStatus);
 
     // This is WRONG - there is a better way to get access to the services - Injection!!
     // const service = new LoggingService();
